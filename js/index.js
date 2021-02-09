75c86b5641847716da90a2786768b96a182b6ca6
Mitchell Walker <eagleye1144@gmail.com>
2021-02-09 15:28:22 -0700

    Three

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
-const nav =  document.querySelector('.nav')
+const nav =  document.querySelector('.nav');
+const images = document.querySelectorAll('img');
+const funBus = document.querySelectorAll('.intro p');
 
 nav.addEventListener("mouseover", (event) => {
     event.target.style.color = "orange";
@@ -8,3 +10,20 @@ nav.addEventListener("mouseover", (event) => {
         event.target.style.color = "";
       }, 500);
     }, false);
+  
+    Array.from(funBus).forEach(elem => {
+        elem.addEventListener('dblclick', event => {
+            event.target.style.fontStyle = 'italic';
+        });
+    });
+
+    images.forEach(item => {
+        item.addEventListener('mousemove', () => {
+           item.style.opacity = '0.4';
+
+           setTimeout(function() {
+            item.style.opacity = "";
+          }, 500);
+        }, false);
+        
+     });
