f2cfee34d0f480c781266178ca97a782bc285d3d
Mitchell Walker <eagleye1144@gmail.com>
2021-02-09 14:06:27 -0700

    1st

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const nav =  document.querySelector('.nav')
+
+nav.addEventListener("mouseover", (event) => {
+    event.target.style.color = "orange";
+
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
